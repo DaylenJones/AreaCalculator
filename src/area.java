e2da5e550c02ff67611a6a178b97a4ea349ed366
Daylen Jones <jonesday001@warren.k12.in.us>
2024-09-12 13:24:49 -0400

    update

--- src/area.java
+++ src/area.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class area {
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(area(5.0));
+        System.out.println(area(-1));
+        System.out.println(area(5.0, 4.0));
+        System.out.println(area(-1.0, 4.0));
+
+    }
+
+    private static double area(double x, double y) {
+        if (radius < 0) {
+            return -1.0;
+        }
+        return Math.PI * radius * radius;
+    }
+
+    private static double area(double radius) {
+        if (x < 0 || y < 0) {
+            return -1.0;
+        }
+        return x * y;
+    }
 }
+
